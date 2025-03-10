bb13894a3d23a2f179d852aa98c8e5c1993b662c
profWagnerToth <wagnertoth@outlook.com>
2025-03-10 19:00:33 -0300

    atualização computador novo

--- DSM3/MongoDB/Biblioteca/biblioteca.js
+++ DSM3/MongoDB/Biblioteca/biblioteca.js
@@ -4,7 +4,7 @@ const {MongoClient} = require('mongodb');
 //Função principal
 async function main() {
     //Definir a URI de conexão com o MongoDB
-    const uri = "mongodb://localhost:27017";
+    const uri = "mongodb://127.0.0.1:27017";
     //Criar instancia do cliente mongoDB
     const client = new MongoClient(uri);
 
